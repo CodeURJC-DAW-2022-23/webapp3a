ab4969c98682a511db92eeae3f8e44bc7529f8ec
Cassiel123 <4610@alumnoaltamira.es>
2023-02-20 20:30:01 +0100

    correccion paquete director controller

--- backend/movieframe/src/main/java/es/webapp3/movieframe/controller/director_controller.java
+++ backend/movieframe/src/main/java/es/webapp3/movieframe/controller/director_controller.java
@@ -1,4 +1,4 @@
-package com.example1.demo;
+package es.webapp3.movieframe.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
