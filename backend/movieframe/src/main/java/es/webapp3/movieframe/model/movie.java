195ecb843a4085a6ebcae077ec74768b4f42eb59
Cassiel123 <4610@alumnoaltamira.es>
2023-02-23 11:59:30 +0100

    creacion entity movie

--- backend/movieframe/src/main/java/es/webapp3/movieframe/model/movie.java
+++ backend/movieframe/src/main/java/es/webapp3/movieframe/model/movie.java
@@ -1,10 +1,70 @@
 package es.webapp3.movieframe.model;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class movie {
     
     @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id = null;
     String title;
+    String director;
+    String gender;
+
+    @Column(columnDefinition = "TEXT")
+    String description;
+
+    public movie(){}
+
+    public movie( String title, String director, String gender, String description) {
+        this.title = title;
+        this.director = director;
+        this.gender = gender;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
 }
