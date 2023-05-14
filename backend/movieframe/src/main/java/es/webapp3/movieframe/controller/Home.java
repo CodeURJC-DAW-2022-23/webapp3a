9f41038012bc39dd7945c69ac07fb5e88bbcc294
MaAlami2020 <ma.alami.2020@alumnos.urjc.es>
2023-05-14 22:12:22 +0200

    corregidos errores de ficheros al cambiar de rama

--- backend/movieframe/src/main/java/es/webapp3/movieframe/controller/Home.java
+++ backend/movieframe/src/main/java/es/webapp3/movieframe/controller/Home.java
@@ -3,7 +3,6 @@ package es.webapp3.movieframe.controller;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.Optional;
 
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
